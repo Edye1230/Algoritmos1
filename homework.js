220b9191168f822ea951282564712de4ed7d85d0
Edye1230 <edye.1230@gmail.com>
2022-04-06 09:11:28 -0500

    Segundo commit Homework Algoritmos 1

--- homework.js
+++ homework.js
@@ -7,14 +7,14 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
   let arreglo = [1];
-  let memoria = num;
-  for (let i = 2; i <= num; i++) {
-    do {
-      if (num % i == 0 && memoria % i == 0) {
-        memoria /= i;
-        arreglo.push(i);
-      }
-    } while (memoria % i == 0);
+  let divisor = 2;
+  while (num !== 1) {
+    if (num % divisor == 0) {
+      arreglo.push(divisor);
+      num /= divisor;
+    } else {
+      divisor++;
+    }
   }
   return arreglo;
 }
@@ -45,6 +45,18 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
+  for (let i = 1; i < array.length; i++) {
+    let aux = array[i];
+    for (let j = 0; j < i; j++) {
+      if (aux < array[j]) {
+        aux = array[j];
+        array[j] = array[i];
+        array[i] = aux;
+      }
+    }
+  }
+  return array;
 }
 
 function selectionSort(array) {
@@ -52,6 +64,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
+  for (let i = 0; i < array.length; i++) {
+    let min = array[i];
+    let current = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (min > array[j]) {
+        min = array[j];
+        current = j;
+      }
+    }
+    if (current != i) {
+      array[current] = array[i];
+      array[i] = min;
+    }
+  }
+  return array;
 }
 
 // No modificar nada debajo de esta línea
